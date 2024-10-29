4296028843e527716a5610c077f02a5f07b42aa4
appsoft-lucio <luciocdesouza@gmail.com>
2024-10-29 14:50:11 -0300

    Adicionei a API ao projeto agenda-mobile

---
+++ App.js
@@ -0,0 +1,17 @@
+import { View } from "react-native";
+import Login from "./src/screens/login/login";
+import Account from "./src/screens/account/account";
+import Home from "./src/screens/home/home";
+import Calendar from "./src/screens/calendar/calendar";
+import Profile from "./src/screens/profile/profile.jsx";
+import Main from "./src/screens/main/main";
+import Services from "./src/screens/services/services.jsx";
+import Schedule from "./src/screens/schedule/schedule.jsx";
+
+export default function App() {
+  return (
+    <View style={{ flexGrow: 1 }}>
+      <Schedule />
+    </View>
+  );
+}
