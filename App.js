aa78f87d21f30c2ae4a5f2c9e6c067eba549e5ca
appsoft-lucio <luciocdesouza@gmail.com>
2024-11-29 06:29:33 -0300

    Ajuste na importação  profile

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Login from "./src/screens/login/login";
 import Account from "./src/screens/account/account";
 import Home from "./src/screens/home/home";
 import Calendar from "./src/screens/calendar/calendar";
-import Profile from "./src/screens/profile/profile.jsx";
+import Profile from "./src/screens/profile/profile";
 import Main from "./src/screens/main/main";
 import Services from "./src/screens/services/services.jsx";
 import Schedule from "./src/screens/schedule/schedule.jsx";
